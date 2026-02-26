c7a60abd02818269deb21c8c2b2167e874b8cdd5
Arthurigorr <arthur.igorr@gmail.com>
2026-02-26 13:32:48 -0300

    Removing unnecessary checks from internal function getSampleBins

--- R/internalFunctions.R
+++ R/internalFunctions.R
@@ -586,12 +586,8 @@ getSampleBins <- function(samples, samp.rate, binSize) {
   b <- seq(1, samples, by = samp.rate * binSize)
   e <- pmin(b + samp.rate * binSize - 1, samples)
 
-  keepThese <- ((samp.rate * binSize) * 0.1) < e - b
-
-  if (length(b) == length(e)) {
-    data.frame(b, e)[keepThese, ]
-  } else {
-    data.frame(b, e = c(e, samples))
-  }
+  keepThese <- ((samp.rate * binSize) * 0.1) < e - b ## This is so we can keep only bins that are at least 10% the size of the audio's samp.rate
+  data.frame(b, e)[keepThese, ]
 
 }
+
