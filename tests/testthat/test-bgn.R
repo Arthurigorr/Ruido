370a40f7ea50ccb0388fbb742f10dea362b4ad97
Arthurigorr <arthur.igorr@gmail.com>
2026-09-22 23:12:16 -0300

    Creating examples for the bgn() function

---
+++ tests/testthat/test-bgn.R
@@ -0,0 +1,27 @@
+test_that("bgn can input tuneR objects, work with default arguments and alternative arguments", {
+
+  library(tuneR)
+
+  samprate = 12050
+  dur = 60
+  n = samprate * dur
+
+  set.seed(413)
+  noise = rnorm(n)
+
+  fade = seq(1, 0, length.out = n)
+
+  signal = noise * fade
+
+  wave1 = Wave(left = signal, right = signal,
+              samp.rate = samprate,
+              bit = 16)
+  wave2 = mono(wave1)
+
+  expect_type(bgn(wave1), "list")
+  expect_type(bgn(wave1, channel = "mono"), "list")
+  expect_type(bgn(wave2), "list")
+  expect_type(bgn(wave2, wl = 256), "list")
+  expect_error(bgn("made-up-nonsense.png"))
+
+})
