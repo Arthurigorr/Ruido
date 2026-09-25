6630cab900e108f9eff9424a06f307eb1eaee45e
Arthurigorr <arthur.igorr@gmail.com>
2026-09-24 23:18:38 -0300

    Adding an error case to bgNoise to test hamming() function error handling

--- tests/testthat/test-bgNoise.R
+++ tests/testthat/test-bgNoise.R
@@ -53,6 +53,7 @@ test_that("bgNoise() reads tuneR Wave objects", {
   expect_s4_class(bgNoise(wave1, timeBin = 30, channel = "right"), "noise.matrix")
   expect_s4_class(bgNoise(wave1, targetSampRate = 6650), "noise.matrix")
   expect_s4_class(bgNoise(wave1, window = hamming(512)), "noise.matrix")
+  expect_error(bgNoise(wave1, window = hamming(-5)))
   expect_s4_class(bgNoise(wave1, histbreaks = 100), "noise.matrix")
   expect_s4_class(bgNoise(wave1, histbreaks = "Sturges"), "noise.matrix")
   expect_s4_class(bgNoise(wave1, DCfix = FALSE), "noise.matrix")
